8eb411c29bab560c12192a5d33f658c150ed1b75
itsMohitShah <mohit.shah165@gmail.com>
2024-10-24 22:14:03 +0200

    Added .gitignore file and saving images in different folders

--- NMS_experiment.py
+++ NMS_experiment.py
@@ -1,5 +1,4 @@
 import os
-import mohitutils
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
@@ -27,7 +26,11 @@ def generate_similar_bbox(gt_bbox, offset_range):
     while True:
         try:
             new_x2 = random.randint(max(0, gt_bbox[2] - offset_range), min(gt_bbox[2] + offset_range, 758))
+            if new_x2-new_x1 < 10:
+                new_x2+=10
             new_y2 = random.randint(max(0, gt_bbox[3] - offset_range), min(gt_bbox[3] + offset_range, 758))
+            if new_y2-new_y1 < 10:
+                new_y2+=10
         except ValueError:
             new_x2 = 768
             new_y2 = 768
@@ -68,11 +71,17 @@ def xyxytoxywh(bbox):
 
 
 def main(iterations = 5,clearall = False):
-    destination_folder = 'Images'
+    path_images_folder = 'Images'
+    path_bothsuppressed_folder = os.path.join(path_images_folder,'BothSuppressed')
+    path_nonesuppressed_folder = os.path.join(path_images_folder,'NoneSuppressed')
+    path_xyxy_folder = os.path.join(path_images_folder,'XYXY')
     if clearall == True:
-        if os.path.exists(destination_folder):
-            shutil.rmtree(destination_folder)
-    os.makedirs(destination_folder, exist_ok=True)
+        if os.path.exists(path_images_folder):
+            shutil.rmtree(path_images_folder)
+    os.makedirs(path_images_folder, exist_ok=True)
+    os.makedirs(path_bothsuppressed_folder, exist_ok=True)
+    os.makedirs(path_nonesuppressed_folder, exist_ok=True)
+    os.makedirs(path_xyxy_folder, exist_ok=True)
     for i in tqdm(range(iterations)):
         position_x = 20
         position_y = 20
@@ -110,8 +119,7 @@ def main(iterations = 5,clearall = False):
         
         canvas_correctNMS = canvas.copy()
         canvas_customNMS = canvas.copy()        
-        canvas_correctNMS00 = canvas.copy()
-        canvas_customNMS00 = canvas.copy()
+
         cv2.rectangle(canvas, (bbox1[0], bbox1[1]), (bbox1[2], bbox1[3]), (255, 0, 0), 2)  # Similar box 1 in blue
         cv2.rectangle(canvas, (bbox2[0], bbox2[1]), (bbox2[2], bbox2[3]), (0, 0, 255), 2)  # Similar box 2 in red
 
@@ -125,8 +133,7 @@ def main(iterations = 5,clearall = False):
         idx_CorrectNMS = cv2.dnn.NMSBoxes(list_bbox_xywh,list_conf,0.3,0.5)
         idx_CustomNMS = cv2.dnn.NMSBoxes(list_bbox,list_conf,0.3,0.5)
 
-        idx_CorrectNMS00 = cv2.dnn.NMSBoxes(list_bbox_xywh,list_conf,0.3,0.0)
-        idx_CustomNMS00 = cv2.dnn.NMSBoxes(list_bbox,list_conf,0.3,0.0)
+
 
         # Draw the bounding boxes on the canvas
         for idx in range(len(list_bbox)):
@@ -140,41 +147,27 @@ def main(iterations = 5,clearall = False):
             cv2.rectangle(canvas_customNMS, (list_bbox[idx][0], list_bbox[idx][1]), (list_bbox[idx][2], list_bbox[idx][3]), list_colour[idx], 2)  
 
 
-        # Draw the bounding boxes on the canvas
-        for idx in idx_CorrectNMS00:
-            cv2.rectangle(canvas_correctNMS00, (list_bbox[idx][0], list_bbox[idx][1]), (list_bbox[idx][2], list_bbox[idx][3]), list_colour[idx], 2)  
-        # Draw the bounding boxes on the canvas
-        for idx in idx_CustomNMS00:
-            cv2.rectangle(canvas_customNMS00, (list_bbox[idx][0], list_bbox[idx][1]), (list_bbox[idx][2], list_bbox[idx][3]), list_colour[idx], 2)  
-
-
-
-
-
-
-
         # Display the result
         plt.imshow(cv2.cvtColor(canvas, cv2.COLOR_BGR2RGB))
         plt.title(f'{i+1}{iterations}\nBlue: {bbox1}\nRed: {bbox2}\nBoxes with IoU: {iou}', fontsize = 10)
         # plt.show()
         if len(idx_CorrectNMS) == len(idx_CustomNMS):
             if len(idx_CustomNMS) == 2:
-                filename = os.path.join(destination_folder,"NOTSuppressedinBothCases_image_{}.png")
+                filename = os.path.join(path_nonesuppressed_folder,"NOTSuppressedinBothCases_image_{}.png")
             else:
-                filename = os.path.join(destination_folder,"SuppressedinBothCases_image_{}.png")
+                filename = os.path.join(path_bothsuppressed_folder,"SuppressedinBothCases_image_{}.png")
         else:
-            filename = os.path.join(destination_folder,"XYXY_image_{}.png")
+            filename = os.path.join(path_xyxy_folder,"XYXY_image_{}.png")
         filecounter = 0
         while os.path.isfile(filename.format(filecounter)):
             filecounter+=1
         filename = filename.format(filecounter)
 
         finalimage = plt.figure(figsize=(20,20))
-        ax1 = plt.subplot2grid(shape=(4,2),loc=(0,0),colspan=2, rowspan= 2)
-        ax2 = plt.subplot2grid(shape=(4,2),loc=(2,0))
-        ax3 = plt.subplot2grid(shape=(4,2),loc=(2,1))
-        ax4 = plt.subplot2grid(shape=(4,2),loc=(3,0))
-        ax5 = plt.subplot2grid(shape=(4,2),loc=(3,1))
+        ax1 = plt.subplot2grid(shape=(3,2),loc=(0,0),colspan=2, rowspan= 2)
+        ax2 = plt.subplot2grid(shape=(3,2),loc=(2,0))
+        ax3 = plt.subplot2grid(shape=(3,2),loc=(2,1))
+
 
         ax1.imshow(cv2.cvtColor(canvas, cv2.COLOR_BGR2RGB))
         ax2.set_title("IoU = 0.5")
@@ -182,10 +175,7 @@ def main(iterations = 5,clearall = False):
         ax3.set_title("IoU = 0.5")
         ax3.imshow(cv2.cvtColor(canvas_customNMS, cv2.COLOR_BGR2RGB))
 
-        ax4.set_title("IoU = 0.0")
-        ax4.imshow(cv2.cvtColor(canvas_correctNMS00, cv2.COLOR_BGR2RGB))
-        ax5.set_title("IoU = 0.0")
-        ax5.imshow(cv2.cvtColor(canvas_customNMS00, cv2.COLOR_BGR2RGB))
+
 
         # plt.imsave(filename,finalimage)
 
